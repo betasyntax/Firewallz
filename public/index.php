7e10cc1069b4e92d1cb1deaf3433b0faff97b672
Donovan Cameron <pynewbie@gmail.com>
2016-08-01 23:41:51 -0400

    adding autoload

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ ini_set('display_errors', 1);
 // require dirname(__FILE__) . '/../boot/app.new.php';
 // 
 
-require __DIR__.'/../boot/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../boot/app.new.php';
