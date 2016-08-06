8527c72561689947daab48bc972a7bafaf6944cb
Donovan Cameron <pynewbie@gmail.com>
2016-08-06 04:27:23 -0400

    test

--- public/index.php
+++ public/index.php
@@ -2,11 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "<pre>";
 // require dirname(__FILE__) . '/../boot/app.new.php';
 // 
 
 require __DIR__.'/../vendor/autoload.php';
+echo "<pre>";
 $app = require_once realpath(__DIR__.'/../boot/app.new.php');
-var_dump($app);
+// $app->make();
+
+// var_dump($app);
+
+// $test = $app->get('view');
+
+// var_dump($test);
 echo "</pre>";
