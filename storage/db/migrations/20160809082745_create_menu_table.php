1eb524f13727d89a9c3a9b86d14abd5e403fada3
Donovan Cameron <pynewbie@gmail.com>
2016-08-09 05:15:04 -0400

    add default wayfinder migration

---
+++ storage/db/migrations/20160809082745_create_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateMenuTable extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $menus = $this->table('menus');
+        $menus->addColumn('parent_id', 'integer', array('limit' => 11,'default' => 1))
+              ->addColumn('title', 'string', array('limit' => 45))
+              ->addColumn('url', 'string', array('limit' => 255,'null' => true))
+              ->addColumn('slug', 'string', array('limit' => 45))
+              ->addColumn('type', 'string', array('limit' => 45,'default' => 'internal'))
+              ->addColumn('status', 'string', array('limit' => 45,'default' => 'enabled'))
+              ->addColumn('site_order', 'string', array('limit' => 45,'default' => '0'))
+              ->addColumn('updated_at', 'datetime', array('null' => true))
+              ->addColumn('created_at', 'datetime', array('null' => true))
+              ->addIndex(array('title', 'url', 'slug'), array('unique' => true))
+              ->save();
+        // }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('menus');
+    }
+
+}
---
+++ storage/db/seeds/MenuTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class MenuTableSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = array(
+          array(
+              'parent_id'    => 0,
+              'title'        => 'Home',
+              'url'          => '',
+              'slug'         => 'home',
+              'type'         => 'internal',
+              'status'       => 'enabled',
+              'site_order'   => '0',
+              'updated_at'   => date('Y-m-d H:i:s'),
+              'created_at'   => date('Y-m-d H:i:s')
+          ),
+          array(
+              'parent_id'    => 0,
+              'title'        => 'Welcome',
+              'url'          => 'welcome',
+              'slug'         => 'welcome',
+              'type'         => 'internal',
+              'status'       => 'enabled',
+              'site_order'   => '0',
+              'updated_at'   => date('Y-m-d H:i:s'),
+              'created_at'   => date('Y-m-d H:i:s')
+          )
+        );
+
+        $posts = $this->table('menus');
+        $posts->insert($data)
+              ->save();
+    }
+}
